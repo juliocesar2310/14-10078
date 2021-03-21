b89e9802a9f41b041ac2e47a175a5a707c782fe7
juliocesar2310 <forwer10@gmail.com>
2021-03-21 17:50:33 -0400

    Modificacion de archivo python

--- asignacion7.py
+++ asignacion7.py
@@ -6,3 +6,18 @@ response = requests.post(
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
 payload=response.json()
 pprint(payload)
+headers = {
+           'X-Auth-Token' : payload.get('Token')
+          }
+response = requests.get("https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device",headers=headers)
+payload2=response.json()
+pprint(payload2)
+
+payload3=payload2.get('response')
+
+for org in payload3:
+    List.append(dict(family=org.get('family'), hostname=org.get('hostname'),
+     managementIpAddress=org.get('managementIpAddress'),
+      lastUpdated=org.get('lastUpdated'), reachabilityStatus=org.get('reachabilityStatus')))
+pprint(List)
+
